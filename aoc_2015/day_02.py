f7d3fce035f757d836b2deedc41c002ade545059
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-09-11 13:00:50 -0500

    Refactor calculator to remember presents

--- aoc_2015/day_02.py
+++ aoc_2015/day_02.py
@@ -1,7 +1,7 @@
 """
 Day 2: I Was Told There Would Be No Math
 
-Part 1:
+--- Part 1 ---
 
 The elves are running low on wrapping paper, and so they need to submit
 an order for more. They have a list of the dimensions (length l,
@@ -16,6 +16,20 @@ each present: the area of the smallest side.
 
 All numbers in the elves' list are in feet. How many total square feet
 of wrapping paper should they order?
+
+--- Part Two ---
+
+The elves are also running low on ribbon. Ribbon is all the same width,
+so they only have to worry about the length they need to order, which
+they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around
+its sides, or the smallest perimeter of any one face. Each present also
+requires a bow made out of ribbon as well; the feet of ribbon required
+for the perfect bow is equal to the cubic feet of volume of the
+present. Don't ask how they tie the bow, though; they'll never tell.
+
+How many total feet of ribbon should they order?
 """
 from dataclasses import dataclass
 from typing import Iterator, List
@@ -39,26 +53,36 @@ class Present:
             + 2 * self.width_wise_area
             + 2 * self.height_wise_area
         )
+        length_wise_perimeter = 2 * self.length + 2 * self.width
+        width_wise_perimeter = 2 * self.width + 2 * self.height
+        height_wise_perimeter = 2 * self.height + 2 * self.length
+        perimeters = {
+            length_wise_perimeter,
+            width_wise_perimeter,
+            height_wise_perimeter,
+        }
+        self.shortest_perimeter = min(perimeters)
 
     def __iter__(self) -> Iterator:
         """Provide unpackable dimensions."""
         return iter((self.length, self.width, self.height))
 
 
+@dataclass
 class Calculator:
     """Calculator to measure total square feet of wrapping paper for
     a list of presents.
     """
 
-    @classmethod
-    def wrapping_paper(cls, presents: List[Present]) -> int:
+    presents: List[Present]
+
+    def calculate_wrapping_paper(self) -> int:
         """Return total needed square feet of wrapping paper for the
         presents.
         """
-        return sum(cls._calculate_square_feet(present) for present in presents)
+        return sum(self._calculate_square_feet(present) for present in self.presents)
 
-    @classmethod
-    def _calculate_square_feet(cls, present: Present) -> int:
+    def _calculate_square_feet(self, present: Present) -> int:
         """Return total needed square feet of wrapping paper for a
         present.
         """
@@ -74,4 +98,4 @@ if __name__ == "__main__":
             for dimensions in data
         ]
 
-    print(Calculator.wrapping_paper(presents))
+    print(Calculator(presents).calculate_wrapping_paper())
--- tests/aoc_2015/test_day_02.py
+++ tests/aoc_2015/test_day_02.py
@@ -7,11 +7,12 @@ def test_present_knows_dimensions():
     length, width, height = present
     assert dimensions == (length, width, height)
     assert present.total_area == 52
+    assert present.shortest_perimeter == 10
 
 
 def test_calculator_calculates_needed_wrapping_paper_for_presents():
     present = Present(2, 3, 4)
     tall_present = Present(1, 1, 10)
-    assert Calculator.wrapping_paper([present]) == 58
-    assert Calculator.wrapping_paper([tall_present]) == 43
-    assert Calculator.wrapping_paper([present, tall_present]) == 101
+    assert Calculator([present]).calculate_wrapping_paper() == 58
+    assert Calculator([tall_present]).calculate_wrapping_paper() == 43
+    assert Calculator([present, tall_present]).calculate_wrapping_paper() == 101
