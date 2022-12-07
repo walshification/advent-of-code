f9c2c89d2ca85dbae9764749db15233ff3e715ad
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-12-06 20:22:01 -0600

    Receiver returns None if no message

--- tests/aoc_2022.py/test_day_06.py
+++ tests/aoc_2022.py/test_day_06.py
@@ -5,7 +5,11 @@ from aoc_2022.day_06 import Receiver
 
 @pytest.mark.parametrize(
     ("signal", "marker"),
-    (("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7), ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)),
+    (
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("aaaaaaaa", None),
+    ),
 )
 def test_receiver_finds_marker(signal, marker):
     assert Receiver(signal).find_marker() == marker
