697a2ffb8c36decbae85d6e8dc06a55407436c82
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-12-20 17:24:11 -0600

    Establish links between passable terrain cells

--- aoc_2022/day_12.py
+++ aoc_2022/day_12.py
@@ -27,8 +27,8 @@ than the elevation of your current square.)
 What is the fewest steps required to move from your current position to
 the location that should get the best signal?
 """
-from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from dataclasses import dataclass, field
+from typing import Iterator, List, Optional, Set, Tuple
 
 CHAR_TO_HEIGHT_MAP = {
     c: n
@@ -43,6 +43,18 @@ class Cell:
     y: int
     x: int
     height: str
+    north: Optional["Cell"] = None
+    east: Optional["Cell"] = None
+    south: Optional["Cell"] = None
+    west: Optional["Cell"] = None
+    links: Set["Cell"] = field(default_factory=set)
+
+    def __hash__(self) -> int:
+        return hash(f"({self.y}, {self.x})")
+
+    def __iter__(self) -> Iterator[int]:
+        """Return a tuple of the cell's row and column positions."""
+        return (i for i in (self.y, self.x))
 
     def __str__(self) -> str:
         return self.height
@@ -50,6 +62,26 @@ class Cell:
     def __xor__(self, other: "Cell") -> bool:
         """Return if one can move from cell to other."""
         return CHAR_TO_HEIGHT_MAP[self.height] >= CHAR_TO_HEIGHT_MAP[other.height] - 1
+
+    @property
+    def neighbors(self) -> Tuple["Cell", ...]:
+        return tuple(
+            cell
+            for cell in (self.north, self.east, self.south, self.west)
+            if cell is not None
+        )
+
+    def is_linked(self, other: "Cell") -> bool:
+        """Return whether cell is linked to other."""
+        return other in self.links
+
+    def link(self, other: "Cell") -> None:
+        """Establish a link between this cell and another.
+
+        Args:
+            other: the cell to link.
+        """
+        self.links.add(other)
 
 
 @dataclass
@@ -59,6 +91,9 @@ class Terrain:
     grid: List[List[Cell]]
     start: Cell
     end: Cell
+
+    def __post_init__(self) -> None:
+        self.establish_links()
 
     @classmethod
     def from_input(cls, heightmap: Tuple[Tuple[str, ...], ...]) -> "Terrain":
@@ -87,5 +122,27 @@ class Terrain:
 
         return self.grid[y][x]
 
+    def __iter__(self) -> Iterator:
+        """Return each cell of the grid."""
+        return (cell for row in self.grid for cell in row)
+
     def __str__(self) -> str:
         return "\n".join(" ".join(str(cell) for cell in row) for row in self.grid)
+
+    def establish_links(self) -> None:
+        """Create links between passable terrain cells."""
+        for cell in self:
+            self.link(cell)
+
+    def link(self, cell: Cell) -> None:
+        """Configure directions."""
+        row, column = cell
+
+        cell.north = self[row - 1, column]
+        cell.south = self[row + 1, column]
+        cell.east = self[row, column + 1]
+        cell.west = self[row, column - 1]
+
+        for neighbor in cell.neighbors:
+            if cell ^ neighbor:
+                cell.link(neighbor)
--- tests/aoc_2022/test_day_12.py
+++ tests/aoc_2022/test_day_12.py
@@ -32,3 +32,18 @@ def test_terrain_can_be_made():
     assert terrain[0, 3] is None
 
     assert str(terrain) == dedent("S a b\n" "c d E")
+
+    assert terrain[0, 0].north == None
+    assert terrain[0, 0].east == terrain[0, 1]
+    assert terrain[0, 0].south == terrain[1, 0]
+    assert terrain[0, 0].west == None
+
+    assert terrain[1, 2].north == terrain[0, 2]
+    assert terrain[1, 2].east == None
+    assert terrain[1, 2].south == None
+    assert terrain[1, 2].west == terrain[1, 1]
+
+    assert terrain.start.is_linked(terrain[0, 1]) == True
+    assert terrain.start.is_linked(terrain[1, 0]) == False
+
+    assert terrain[1, 0].is_linked(terrain.start) == True
